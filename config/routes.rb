e5ffd438ebf14b344e5cce39a639d08a7a874c7d
lovemercybiz <lovemercybiz@gmail.com>
2018-09-16 14:07:47 -0700

    Udemy Rails Course Sect 3

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  # get 'pages/home', to: 'pages#home'
+  # get 'pages/about', to: 'pages#about' #would be links like 3000/pages/about
+end
