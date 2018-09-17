a268c8acc5c8a3a72afb8c89b1448b8f24dc0fb8
lovemercybiz <lovemercybiz@gmail.com>
2018-09-17 17:34:47 -0500

    Removed redundency and improved for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -9,11 +11,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        set_article#@article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
+        set_article # @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Updated!"
             redirect_to article_path(@article)
@@ -35,17 +37,21 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
+        set_article # @article = Article.find(params[:id])
     end
 
     def destroy
-        @article = Article.find(params[:id])
+        set_article #@article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article deleted"
         redirect_to articles_path
     end
 
     private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
