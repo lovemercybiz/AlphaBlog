bc4aaa7321b8318bae3307eccd3da7771c362c36
lovemercybiz <lovemercybiz@gmail.com>
2018-09-17 16:29:33 -0500

    Added articles edit, update, show and index functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Updated!"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
     end
 
     def create
